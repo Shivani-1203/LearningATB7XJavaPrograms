fd0fbee7a5b36bb58a30cee55c34e14f1e7739dd
Paithara Shivani <paitharashivani@gmail.com>
2024-07-14 00:14:35 +0530

    Changes added

--- src/ex_13072024/Lab016.java
+++ src/ex_13072024/Lab016.java
@@ -2,11 +2,11 @@ package ex_13072024;
 
 public class Lab016 {
     public static void main(String[] args) {
-        char c1 = '\n'; // New Line
+        // char c1 = '\n'; // New Line
 
-        char c2 = '\t'; //tab
+        // char c2 = '\t'; //tab
 
-        char c3 = '\b'; //backslash - deletes one char
+        // char c3 = '\b'; //backslash - deletes one char
 
         char c4 = '\r'; //backslash -- deletes one word
 
