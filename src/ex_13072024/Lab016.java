e7c46f6176692b39d1ab2ec6d777604ecb93e3ad
Paithara Shivani <paitharashivani@gmail.com>
2024-07-14 00:12:15 +0530

    Changes added

---
+++ src/ex_13072024/Lab016.java
@@ -0,0 +1,17 @@
+package ex_13072024;
+
+public class Lab016 {
+    public static void main(String[] args) {
+        char c1 = '\n'; // New Line
+
+        char c2 = '\t'; //tab
+
+        char c3 = '\b'; //backslash - deletes one char
+
+        char c4 = '\r'; //backslash -- deletes one word
+
+
+
+        System.out.println("Shivani" + c4);
+    }
+}
